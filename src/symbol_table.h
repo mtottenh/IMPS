a5ec27e321cf79f9fdd52a2c084032e918a0060b
Max Tottenham <mt3410@imperial.ac.uk>
2012-06-01 12:50:35 +0100

    added symbol_table.h to be tracked by git

---
+++ src/symbol_table.h
@@ -0,0 +1,13 @@
+#ifndef _SYMBOL_TABLE_H_
+#define _SYMBOL_TABLE_H_
+
+symbol_table_new(void);
+symbol_table_free();
+symbol_table_getLength();
+symbol_table_put();
+symbol_table_remove();
+symbol_table_contains();
+symbol_table_get();
+symbol_table_map();
+
+#endif
