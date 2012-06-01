d2b97a26f4dcf74ca8c35ddb9f53cbb23517e9f4
Max Tottenham <mt3410@imperial.ac.uk>
2012-06-01 15:38:57 +0100

    symbol_table.h: fixed warnings and errors in typedefs to get it to compile, symbol_table.c: Tested new() and getLength() both appear to work well

--- src/symbol_table.h
+++ src/symbol_table.h
@@ -1,25 +1,31 @@
 #ifndef _SYMBOL_TABLE_H_
 #define _SYMBOL_TABLE_H_
+#include <stdint.h>
+#include <string.h>
 /* THINGS TO CONSIDER:
  * 1. Hashtable implementation of symbol table
  * 2. Binary tree implementation?
  */
 
 /* Types for symbol table*/
-typedef Key char*;
-typedef Value uint16_t;
-
-typedef struct Symbol_Table {
-	Symbol_Table_Entry *head;
-}
+typedef char* Key;
+typedef uint16_t Value;
 /* Linekd list implementation of symbol table */
-typedef struct Symbol_Table_Entry {
+typedef struct  Symbol_Table_Entry {
 	Key key;
 	Value value; 
-	Symbol_Table *next;
-};
+	struct Symbol_Table_Entry *next;
+} Symbol_Table_Entry;
 
-/* Creates a new symbol table on the head returning a pointer*/
+
+typedef struct {
+	Symbol_Table_Entry *head;
+} Symbol_Table; 
+/* 
+ * Creates a new symbol table on the heap
+ * containing opcode and directive mappings
+ * returns a pointer
+ */
 Symbol_Table* Symbol_Table_new(void);
 
 /* Deletes a symbol table */
@@ -32,25 +38,25 @@ void Symbol_Table_free(Symbol_Table*);
 size_t Symbol_Table_getLength(Symbol_Table*);
 
 /* Adds the (key,value) pair to the symbol table poitned to by Symbol_Table* */
-int Symbol_Table_put(Symbol_Table*,key,value);
+int Symbol_Table_put(Symbol_Table*,Key,Value);
 
 /* 
  * Removes all (key,_) pairs from the symbol table 
  * pointed to by Symbol_Table* 
  */
-int Symbol_Table_remove(Symbol_Table*,key);
+int Symbol_Table_remove(Symbol_Table*,Key);
 
 /*
  * Checks to see if a (key,_) pair is in the symbol table 
  * pointed to by Symbol_Table* 
  */
-int Symbol_Table_contains(Symbol_Table*,key);
+int Symbol_Table_contains(Symbol_Table*,Key);
 
 /*
  * Returns the first (key,value) pair in the symbol table
  * pointed to by Symbol_Table* 
  */
-value Symbol_Table_get(Symbol_Table*,key);
+Value Symbol_Table_get(Symbol_Table*,Key);
 
 /*Symbol_Table_map();*/
 
