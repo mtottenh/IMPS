83ebcd5ceff4e63076455ae73fd26639af90bc07
Sean Allan <sra11@doc.ic.ac.uk>
2012-06-04 18:38:53 +0100

    pass2.c pass2.h - Completed code for pass 2. Testing remains to be done once symbol_table is complete.

--- src/pass2.h
+++ src/pass2.h
@@ -3,13 +3,14 @@ typedef struct Operands {
 	uint32_t operand1;
 	uint32_t operand2;
 	uint32_t operand3;
+	
 };
 
-typedef void (*FunctionPointer)(Tokeniser_Line*);
+typedef void (*FunctionPointer)(uint32_t, Tokeniser_Line*, Symbol_Table);
 
-void pass2(FILE*, Symbol_Table table);
-int16_t eval_immediate(char*);
-uint8_t eval_register(char*);
+void pass2(FILE*, FILE*, Symbol_Table*);
+uint32_t eval_immediate(char*, uint32_t, Symbol_Table*);
+uint32_t eval_register(char*);
 
 uint32_t assemble_halt(Tokeniser_Line*);
 uint32_t assemble_rtype(Tokeniser_Line*);
