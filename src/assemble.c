f4db675de95a8f94a33d644002aeea106f7b4a36
Sean Allan <sra11@doc.ic.ac.uk>
2012-06-08 16:45:13 +0100

    Refactoring and merging branches. (Sean, Max & Mitchell)

--- src/assemble.c
+++ src/assemble.c
@@ -1,42 +1,33 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "symbol_table.h"
+#include "pass1.h"
+#include "pass2.h"
 
-int main(int argc, char **argv) {
-    if (argc != 3) {
-        /*If not executed in the form ./assemble <input file> <output file> error and quit*/
-        printf("\nUsage: %s <input file> <output file>\n", argv[0]);
-        return EXIT_FAILURE;
-    } else {
-        /*If we got proper args, try to open the input file for reading*/
-        FILE *input;
+int main(int argc, char** argv) {
+	/* Get input and output file pointers from command line arguments. */
+	FILE* input = fopen(argv[1], "rt");
+	if (input == NULL) {
+		perror("Could not open input file. Terminating.");
+		exit(EXIT_FAILURE);
+	}
 
-	if ((input = fopen(argv[1],"r")) == NULL) {
-            printf("Error: Could not open file: %s ", argv[1]);
-            return EXIT_FAILURE;
-        }
+	FILE* output = fopen(argv[2], "wb+");
+	if (output == NULL) {
+		perror("Could not open output file. Terminating.");
+		exit(EXIT_FAILURE);
+	}
 
-        /* Opens the output file to write to in binary mode. */
-        char *output_path = argv[2];
-        FILE *output;
-        if ((output = fopen(output_path, "wb")) == NULL)
-        {
-                perror("Output file couldn't be opened");
-                exit(EXIT_FAILURE);
-        }
+	/* Create a new symbol table for the two passes. */
+	Symbol_Table* table = symbol_table_new();
 
+	/* Perform pass 1, then reopen the input file and perform pass 2. */
+	pass1(input, table);
+	fclose(input);
+	
+	input = fopen(argv[1], "rt");
+	pass2(input, output, table);
 
-        /* 1. Read through the binary file and construct the symbol table*/
-            /* 1.1 Tokenize a line and put into  a stuct? similar to what we did with operands?*/
-            /* 1.2 Each line in an assembly file is 4 addresses starting from 0 (as byte adressable memory and each line is 32 bytes)*/
-            /* 1.3 Every time we encounter a label. Add it to the symbol table and add its corresponding address*/
-        /* 2. Read through the binary file and construct the binary output using symbol table*/
-            /* 2.1 Consider creating 3 funcs to deal with the 3 different instruction formats?*/
-        /* 3. Write binary output to the file specified by <output file>*/
-            /* 3.1 What are we going to use to store the temp output of an assembler thing? just a char array of MEM_SIZE?*/
-    }
+	/* Close both file streams and exit. */
+	fclose(input);
+	fclose(output);
 
-
-    return EXIT_SUCCESS;
-    
+	return EXIT_SUCCESS;
 }
